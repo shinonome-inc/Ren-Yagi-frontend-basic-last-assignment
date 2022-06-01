9a1050f0273a379eee2abb866ebd3e7cbdb13fbc
Ren Yagi <r.yagi@4nonome.com>
2022-06-01 17:38:16 +0900

    message2

---
+++ src/stories/molecules-message2.stories.js
@@ -0,0 +1,6 @@
+import Item_Message2 from "../components/molecules/message2/message2.html";
+import "../components/molecules/message2/message2.scss";
+
+export default { title: "Molecules/Message2" };
+
+export const Message = () => Item_Message2;

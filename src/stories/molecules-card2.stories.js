de222b7d1da6093e97e70366cbf450f60212e13d
Ren Yagi <r.yagi@4nonome.com>
2022-06-13 13:52:43 +0900

    レビューお願いします３

---
+++ src/stories/molecules-card2.stories.js
@@ -0,0 +1,6 @@
+import Item_Card2 from "../components/molecules/card2/card2.html";
+import "../components/molecules/card2/card2.scss";
+
+export default { title: "Molecules/Card2" };
+
+export const Example = () => Item_Card2;

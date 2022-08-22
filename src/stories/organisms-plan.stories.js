f3c72e28942b3030862224a3e4da5e447c5c9387
Ren Yagi <r.yagi@4nonome.com>
2022-08-22 13:32:38 +0900

    html,css,storiesの追加

---
+++ src/stories/organisms-plan.stories.js
@@ -0,0 +1,6 @@
+import Item_Plan from "../components/organisms/plan/plan.html";
+import "../components/organisms/plan/plan.scss";
+
+export default { title: "Organisms/Plan" };
+
+export const Events = () => Item_Plan;

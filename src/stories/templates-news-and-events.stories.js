1bab17090bd5388d8274a63ade5eeca157751fd3
Ren Yagi <r.yagi@4nonome.com>
2022-09-13 15:20:18 +0900

    誤字の修正

--- src/stories/templates-news-and-events.stories.js
+++ src/stories/templates-news-and-events.stories.js
@@ -1,6 +1,6 @@
 import Item_Newsandevents from "../components/templates/news-and -events/newsandevents.html";
 import "../components/templates/news-and -events/newsandevents.scss";
 
-export default { title: "Templates/Newsamdevents" };
+export default { title: "Templates/Newsandevents" };
 
 export const Events = () => Item_Newsandevents;

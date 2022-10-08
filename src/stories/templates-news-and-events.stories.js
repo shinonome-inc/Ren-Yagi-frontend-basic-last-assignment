125da27ef31d7fbfb97a809566a4e90d7b022453
Ren Yagi <r.yagi@4nonome.com>
2022-10-08 16:49:57 +0900

    諸々の変更

--- src/stories/templates-news-and-events.stories.js
+++ src/stories/templates-news-and-events.stories.js
@@ -1,6 +1,6 @@
-import Item_Newsandevents from "../components/templates/news-and -events/newsandevents.html";
-import "../components/templates/news-and -events/newsandevents.scss";
+import Item_Newsandevents from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
 
-export default { title: "Templates/Newsandevents" };
+export default { title: "Templates/News-and-events" };
 
 export const Events = () => Item_Newsandevents;
